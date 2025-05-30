c81046afbe1e59210848fa58fbd363e7e782eda6
Sara Myrheim <saramyrheim@hotmail.com>
2025-05-30 23:36:36 +0200

    Kodeloftet oblig 2

---
+++ projects/wireframe_to_web/script.js
@@ -0,0 +1,5 @@
+buttonDiscount.addEventListener("click", function () {
+  const discount = document.getElementById("discount");
+  const buttonDiscount = document.getElementById("buttonDiscount");
+  discount.remove();
+});
